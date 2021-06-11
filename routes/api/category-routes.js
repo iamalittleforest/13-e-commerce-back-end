02037e9756c5f0a0295bf1551957b397c2a3d99b
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-10 23:05:30 -0700

    complete DELETE to delete a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,7 +11,7 @@ router.get('/', async(req, res) => {
     const categoryData = await Category.findAll({
       include: [{ model: Product }]
     });
-    
+
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -66,8 +66,24 @@ router.put('/:id', async(req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// DELETE a category
+router.delete('/:id', async(req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
