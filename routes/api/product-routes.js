77ec878b5bb43b6097ea81d1c5c42766ccc124e3
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-11 21:44:53 -0700

    complete GET for single product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // GET all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   try {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag}]
@@ -18,10 +18,22 @@ router.get('/', (req, res) => {
   }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// GET a single product
+router.get('/:id', async(req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag}]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
