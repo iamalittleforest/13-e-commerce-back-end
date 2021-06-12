d6f265a469311fda31f3948ecd754d7d5193a2db
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-11 21:20:43 -0700

    complete GET for all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
+
+// import models
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// GET all tags
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }]
+    });
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
