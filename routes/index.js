21ecf3d741fcb64ef63c722c9fc9dac5f8a81f96
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-11 22:01:46 -0700

    add comments

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
+
+// import routes
 const apiRoutes = require('./api');
 
+// prefix all routes
 router.use('/api', apiRoutes);
 
+// send message for wrong routes
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
