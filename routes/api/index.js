a99a176d38f229cceffbafad14c9ad51609adc2a
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-11 21:55:11 -0700

    add comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+
+// import routes
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// prefix all routes 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
