6378a4decbc4367d823d95c10d030b92fff87860
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-11 22:12:49 -0700

    refactor comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,5 @@
-const router = require('express').Router();
-
 // import routes
+const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
