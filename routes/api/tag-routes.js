cd01f4e79db57430a508e895e26caab25a2512a1
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-11 21:25:54 -0700

    complete CREATE for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,8 +36,14 @@ router.get('/:id', (req, res) => {
   }
 });
 
+// CREATE a tag
 router.post('/', (req, res) => {
-  // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
